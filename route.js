64b3ec89c78e02970cf27745f8388fb8e9729f50
Israel Gboluwaga Arunah <arunahisraelg2010@gmail.com>
2019-07-27 09:47:57 +0100

    Completed ui, config before test

--- route.js
+++ route.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
+const Attendee = require('./attendee');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index');
 });
+
+router.post('/', async (req, res) => {
+  const { first_name, last_name, email, phone, reference } = req.body;
+  const newAttendee = new Attendee({
+    first_name, last_name, email, phone, reference
+  });
+  try {
+    await newAttendee.save();
+    return res.json({ status: true });
+  }
+  catch (err) {
+    return ({ status: false, data: err });
+  }
+})
 
 module.exports = router;
